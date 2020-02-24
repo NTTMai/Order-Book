5aed2f1f9d77f18cafb40c0ec803158fda18efe9
root <thaiphuongn1@gmail.com>
2020-02-24 17:59:29 +0700

    Updates

---
+++ precache-manifest.22a39be8963652e7e441d705fcdbf129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64e653160d7c55474afe72a502811a2",
+    "url": "/Order-Book/index.html"
+  },
+  {
+    "revision": "cd049e32fd10c386d703",
+    "url": "/Order-Book/static/css/main.a8c2f163.chunk.css"
+  },
+  {
+    "revision": "e78798b7afee0422d168",
+    "url": "/Order-Book/static/js/2.9169105c.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/Order-Book/static/js/2.9169105c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd049e32fd10c386d703",
+    "url": "/Order-Book/static/js/main.01ebe030.chunk.js"
+  },
+  {
+    "revision": "63fa56032b7d83a99409",
+    "url": "/Order-Book/static/js/runtime-main.2018e9b9.js"
+  }
+]);
